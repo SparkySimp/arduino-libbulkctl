b3de1567aa7fea24f5346656b6740a78df555678
Kıvılcım Cemal Öztürk <69117901+SparkySimp@users.noreply.github.com>
2022-11-29 09:22:11 +0300

    Create libbulkctl.h

---
+++ libbulkctl.h
@@ -0,0 +1,27 @@
+#ifndef LIBBULKCTL_H
+#define LIBBULKCTL_H
+#include "Arduino.h"
+
+typedef int PIN_T; // type for pin.
+typedef int VOLTAGE_T; // type for voltage.
+typedef unsigned int DURATION_T; // type of duration.
+typedef int MODE_T; // pin mode
+
+void writePulse(PIN_T pin, DURATION_T timeout) { // Sets pin to HIGH, waits for timeout milliseconds, then sets the pin back to LOW.
+	digitalWrite(pin, HIGH);
+	delay(timeout);
+	digitalWrite(pin, LOW);
+}
+
+void bulkWrite(size_t nPins, PIN_T *pins, VOLTAGE_T *values) { // assigns values to pins.
+	for (size_t iota = 0; iota < nPins; iota++) {
+		digitalWrite(*(pins + iota), *(values + iota));
+	}
+}
+
+void bulkPinMode(size_t nPins, PIN_T *pins, MODE_T *modes) { // sets pins to modes.
+	for (size_t iota = 0; iota < nPins; iota++) {
+		pinMode(*(pins + i), *(modes + i));
+	}
+}
+#endif
